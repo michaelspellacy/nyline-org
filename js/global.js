72fa48287851d46174539c3e5a36a5c182014d9f
Michael Spellacy (Spell) <spell@spellacy.net>
2025-05-07 08:53:26 -0400

    Update global.js

--- js/global.js
+++ js/global.js
@@ -5,28 +5,6 @@
 
 	document.getElementsByTagName("html")[0].className = "js";
 
-	// PWA Register
-
-	if (navigator.serviceWorker.controller) {
-
-	  console.log("Active service worker found, no need to register");
-
-	} else {
-
-		//Register the ServiceWorker
-
-		navigator.serviceWorker.register("/service-worker.js", {
-
-	    	scope: "./"
-
-		}).then(function(reg) {
-
-	    	console.log("Service worker has been registered for scope:" + reg.scope);
-
-		});
-
-	}
-
 	// Helper Functions
 
 	function insertAfter(el, referenceNode) {
